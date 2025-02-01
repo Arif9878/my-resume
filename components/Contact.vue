51b2ebada7cdf7475d091c38aa4f4809edff78a9
Arif Wicaksono <arif.wicaksono@stockbit.com>
2025-02-01 19:12:16 +0700

    chore: update linkedin profile

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,7 +2,7 @@
     <footer class="bg-gray-800 text-white text-center py-6 mt-6">
       <p>Hubungi Saya:</p>
       <div class="flex justify-center gap-4 mt-2">
-        <a href="https://linkedin.com/in/arif-wicaksono" class="text-blue-400 hover:underline">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/arif-wicaksono-3b0a7815a" class="text-blue-400 hover:underline">LinkedIn</a>
         <a href="https://github.com/Arif9878" class="text-blue-400 hover:underline">GitHub</a>
         <a href="mailto:arifboyz16@gmail.com" class="text-blue-400 hover:underline">Email</a>
       </div>
